dcd33ca3bf25c58c9e0ec7d2eda0b55b470ad4f0
franperez22 <franperez891@gmail.com>
2023-10-25 12:56:09 -0300

    itemDEtailContainer

--- src/components/particles/ItemDetailContainer.jsx
+++ src/components/particles/ItemDetailContainer.jsx
@@ -5,7 +5,20 @@ export default function ItemDetailContainer ({id}) {
     const { data } = useFetch(`https://fakestoreapi.com/products/${id}`)
     console.log("data", data)
     return (
-        <div>{data.title}</div>
+        <section className="productDetail flex gap-12 mt-12">
+            <div className="productDetail-img md:w-[400px] w-[40vw]">
+                <img className="w-full h-auto object-cover" src={data.image} alt="" />
+            </div>
+            <div className="productDetail-info flex-grow basis-0 shop">
+                <h2 className="text-2xl font-semibold mb-4">{data.title}</h2>
+                <p className="mb-5">{data.description}</p>
+                <span className="text-[#ff9577] text-lg font-semibold">${data.price}</span>
+                <div className="productDetail-info-buttons mt-5">
+                    <button className="cart">Add to cart</button>
+                    <button className="buy">Buy</button>
+                </div>
+            </div>
+        </section>
     )
 }
 
